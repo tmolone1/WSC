dbc53e9599f61957bbf4b3d4bc7db71c9f5645ab
Tim Moloney <tmoloney@trihydro.com>
2020-11-11 12:45:14 -0700

    edited writeup

--- Nitrate_screening_level.R
+++ Nitrate_screening_level.R
@@ -27,3 +27,39 @@ Hprime <- 0
 rhob <- 1.5
 Ct<-EqnB(MCL, DAF, Kd, thetaw, thetaa, Hprime, rhob)
 Ct
+
+MCL<-0.5 #ammonia, mg/L
+Kd<-0
+thetaw<-0.35
+thetaa <- 0.13
+Hprime <- 0
+rhob <- 1.5
+Ct<-EqnB(MCL, DAF, Kd, thetaw, thetaa, Hprime, rhob)
+Ct
+
+
+###WSC site specifc calculation
+da<-42.7 #depth of aquifer, meters, SIMPLOT
+I<-0.01057 #infiltration, meters/year, SIMPLOT
+K<-6812 #hydraulic conductivity, meters/year, SIMPLOT
+i<-0.0018 #average hydraulic gradient, WSC SITE SPECIFIC
+L<-866 #length of source parallel to groundwater flow, WSC SITE SPECIIFC
+d<-Eqn4(L,da,I,K,i)
+DAF<-DAFEqn(K,i,d,I,L)
+MCL<-10 #nitrate, mg/L
+Kd<-0 #assumed
+thetaw<-0.3 #FACT SHEET 25 default
+thetaa <- 0.13 #FACT SHEET 25 default
+Hprime <- 0 #FACT SHEET 25 default
+rhob <- 1.5 #FACT SHEET 25 default
+Ct<-EqnB(MCL, DAF, Kd, thetaw, thetaa, Hprime, rhob)
+Ct
+
+MCL<-0.5 #ammonia, mg/L
+Kd<-0.82 #L/kg, Trihydro 1995?/2018 - WSC Specific
+thetaw<-0.3 #FACT SHEET 25 default
+thetaa <- 0.13 #FACT SHEET 25 default
+Hprime <- 0 #FACT SHEET 25 default
+rhob <- 1.5 #FACT SHEET 25 default
+Ct<-EqnB(MCL, DAF, Kd, thetaw, thetaa, Hprime, rhob)
+Ct
